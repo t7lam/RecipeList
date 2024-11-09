9c108daf355efac86f92c21902aeb6ca8ef436cf
Tommy Lam <tlam7td@gmail.com>
2024-11-09 01:59:53 -0500

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Tommy Lam on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
